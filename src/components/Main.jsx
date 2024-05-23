71dfc9092ac72076ea91f595486d76f480876d5a
olgakahraman <ol.khrmn@gmail.com>
2024-05-22 23:41:30 -0400

    added half react logo img and set it as a background and set the positioning to it

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,33 +1,20 @@
-import React from "react"
-import "./Main.css"
+import React from "react";
+import reactLogo from "../assets/reactjs-icon2.png";
+import "./Main.css";
 
-export default function Main(){
-    return (
-      <div className="main-content">
-        <h1>Fun facts about React</h1>
+export default function Main() {
+  return (
+    <main className="main-content">
+      <h1>Fun facts about React</h1>
+      
         <ul className="list-items">
-          <li>
-            <span className="list-text">Was first released in 2013</span>
-          </li>
-          <li>
-            <span className="list-text">
-              Was originally created by Jordan Walke
-            </span>
-          </li>
-          <li>
-            <span className="list-text">
-              Has well over 100K stars on GitHub
-            </span>
-          </li>
-          <li>
-            <span className="list-text">Is maintained by Facebook</span>
-          </li>
-          <li>
-            <span className="list-text">
-              Powers thousands of enterprise apps, including mobile apps
-            </span>
-          </li>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100K stars on GitHub</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
         </ul>
-      </div>
-    );
+        
+    </main>
+  );
 }
